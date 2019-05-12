7d8f419a05de902e2777f5a604d8b7d8dab3cf79
succie <exactf1ash503@gmail.com>
2019-05-12 23:59:17 +0900

    Add device store

--- client/store/index.ts
+++ client/store/index.ts
@@ -1,13 +1,16 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { mados, Mado } from "./mado";
+import { Device, device } from "./device";
 
 export type RootState = {
   mados: Mado[];
+  device: Device;
 };
 
 const rootReducer = combineReducers({
-  mados
+  mados,
+  device
 });
 
 export const store = createStore(
