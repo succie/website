b607659f2fc4e1789dd9bbfb3977dad653dc4aaa
succie <exactf1ash503@gmail.com>
2019-05-13 00:10:32 +0900

    モバイルかどうかを判断するアクションをリサイズイベントに追加

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import Header from "./Header/Header";
 import Dock from "./Dock/Dock";
 import Field from "./Field/Field";
+import Mobile from "./Mobile";
+import { RootState } from "../store";
+import { deviceActions } from "../store/device";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Dock />
-      <Field />
-    </div>
-  );
+const mapStateTopProps = (state: RootState) => {
+  return {
+    device: state.device
+  };
 };
 
-export default App;
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    setDeviceType: (isMobile: boolean) =>
+      dispatch(deviceActions.setDeviceType(isMobile))
+  };
+};
+
+type Props = ReturnType<typeof mapStateTopProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+class App extends Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.checkMobile();
+    window.onresize = this.checkMobile;
+  }
+
+  private checkMobile = () => {
+    if (window.innerWidth <= 768 && !this.props.device.isMobile)
+      this.props.setDeviceType(true);
+    else if (window.innerWidth > 768 && this.props.device.isMobile)
+      this.props.setDeviceType(false);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.props.device.isMobile ? (
+          <Mobile />
+        ) : (
+          <>
+            <Header />
+            <Dock />
+            <Field />
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+export default connect(
+  mapStateTopProps,
+  mapDispatchToProps
+)(App);
