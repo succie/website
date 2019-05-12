98f00d3d92e1b278f48597af33528890fdce07d8
succie <exactf1ash503@gmail.com>
2019-05-12 23:59:47 +0900

    Add mobile component

---
+++ client/components/Mobile/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dock from "../../Dock/Dock";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <Dock />
+    </div>
+  );
+};
+
+export default Home;
