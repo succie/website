47e4522f92bab5dc9093504c9e65f7c0639cb209
succie <exactf1ash503@gmail.com>
2019-05-14 12:08:42 +0900

    Madoのモバイル対応

--- client/components/Mobile/Home/Home.tsx
+++ client/components/Mobile/Home/Home.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 import Dock from "../../Dock/Dock";
+import Mado from "../../Mado/Mado";
+import { RootState } from "../../../store";
 
-const Home = () => {
+const mapStateToProps = (state: RootState) => {
+  return {
+    mados: state.mados
+  };
+};
+
+type Props = ReturnType<typeof mapStateToProps>;
+
+const Home = (props: Props) => {
   return (
     <div className="Home">
       <Dock />
+      {props.mados.map(mado => (
+        <Mado key={mado.id} {...mado} numMados={1} isMobile={true} />
+      ))}
     </div>
   );
 };
 
-export default Home;
+export default connect(mapStateToProps)(Home);
