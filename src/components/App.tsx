63a41d95499da9984723cba7c1aa7a739cc323ae
succie <kodai.suyama@gmail.com>
2020-03-08 06:15:45 +0900

    Update App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import Header from './Header/Header';
 import Dock from './Dock/Dock';
 import Field from './Field/Field';
@@ -7,37 +7,14 @@ import Mobile from './Mobile';
 import { RootState } from '../store';
 import { deviceActions } from '../store/device';
 
-const mapStateTopProps = (state: RootState) => {
-  return {
-    device: state.device
-  };
-};
+type Props = {
+  isMobile: boolean;
+} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    setDeviceType: (isMobile: boolean) => dispatch(deviceActions.setDeviceType(isMobile))
-  };
-};
-
-type Props = ReturnType<typeof mapStateTopProps> & ReturnType<typeof mapDispatchToProps>;
-
-const App = ({ device, setDeviceType }: Props) => {
-  const checkMobile = useCallback(() => {
-    if (window.innerWidth <= 768 && !device.isMobile) setDeviceType(true);
-    else if (window.innerWidth > 768 && device.isMobile) setDeviceType(false);
-  }, [device, setDeviceType]);
-
-  useEffect(() => {
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-    () => {
-      window.removeEventListener('resize', checkMobile);
-    };
-  }, [checkMobile]);
-
+const Component: React.FC<Props> = ({ isMobile }) => {
   return (
     <div className="App">
-      {device.isMobile ? (
+      {isMobile ? (
         <Mobile />
       ) : (
         <>
@@ -50,4 +27,30 @@ const App = ({ device, setDeviceType }: Props) => {
   );
 };
 
-export default connect(mapStateTopProps, mapDispatchToProps)(App);
+const isMobileSelector = (state: RootState) => state.device.isMobile;
+
+const Container = () => {
+  const dispatch = useDispatch();
+  const isMobile = useSelector(isMobileSelector);
+
+  const checkMobile = useCallback(() => {
+    if (!isMobile && window.innerWidth <= 768) {
+      dispatch(deviceActions.setDeviceType(true));
+    } else if (isMobile && window.innerWidth > 768) {
+      dispatch(deviceActions.setDeviceType(false));
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, [checkMobile]);
+
+  return <Component isMobile={isMobile} />;
+};
+
+export const App = Container;
